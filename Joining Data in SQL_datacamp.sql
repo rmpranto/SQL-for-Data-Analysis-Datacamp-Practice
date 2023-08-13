2130c37612291a06e69d08f5c20dfd873ca1c658
Rahat Maksud Pranto <49338951+rmpranto@users.noreply.github.com>
2023-08-13 23:58:53 +0600

    Update Joining Data in SQL_datacamp.sql

--- Joining Data in SQL_datacamp.sql
+++ Joining Data in SQL_datacamp.sql
@@ -89,3 +89,115 @@ ON c.code = e.code;
 Outer Joins, Cross Joins and Self Joins- part 2
 ------------------------------------------------
 
+SELECT 
+    c1.name AS city,
+    code,
+    c2.name AS country,
+    region,
+    city_proper_pop
+FROM cities AS c1
+-- Perform an inner join with cities as c1 and countries as c2 on country code
+INNER JOIN countries AS c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;
+
+
+SELECT c1.name AS city, code, c2.name AS country,region, city_proper_pop
+FROM cities AS c1
+-- Join right table (with alias)
+left join countries as c2
+ON c1.country_code = c2.code
+ORDER BY code DESC;\
+
+SELECT name, region, gdp_percapita
+FROM countries AS c
+LEFT JOIN economies AS e
+-- Match on code fields
+ON c.code=e.code
+-- Filter for the year 2010
+where year=2010
+order by year desc;
+
+-- Select region, and average gdp_percapita as avg_gdp
+select region,avg(gdp_percapita) as  avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+-- Group by region
+GROUP BY region;
+
+
+SELECT region, AVG(gdp_percapita) AS avg_gdp
+FROM countries AS c
+LEFT JOIN economies AS e
+USING(code)
+WHERE year = 2010
+GROUP BY region
+-- Order by descending avg_gdp
+order by avg_gdp desc
+-- Return only first 10 records
+limit 10;
+
+--Perform a full join with countries (left) and currencies (right).Filter for the North America region or NULL country names.
+SELECT name AS country, code, region, basic_unit
+FROM countries
+-- Join to currencies
+full join  currencies
+USING (code)
+-- Where region is North America or name is null
+WHERE region='North America' or name is NULL
+ORDER BY region;
+
+
+--Complete the FULL JOIN with countries as c1 on the left and languages as l on the right, using code to perform this join.Next, chain this join with another FULL JOIN, placing currencies on the right, joining on code again.
+SELECT c1.name AS country, region, l.name AS language,basic_unit, frac_unit
+FROM countries as c1 
+-- Full join with languages (alias as l)
+FULL JOIN languages as l 
+USING(code)
+-- Full join with currencies (alias as c2)
+FULL JOIN currencies AS c2
+USING(code)
+WHERE region LIKE 'M%esia';
+
+SELECT c.name AS country, l.name AS language
+-- Inner join countries as c with languages as l on code
+FROM countries AS c
+inner join languages as l
+on c.code=l.code
+WHERE c.code IN ('PAK','IND')
+	AND l.code in ('PAK','IND');
+
+
+SELECT c.name AS country, l.name AS language
+FROM countries AS c        
+-- Perform a cross join to languages (alias as l)
+cross join languages as l
+WHERE c.code in ('PAK','IND')
+	AND l.code in ('PAK','IND');
+
+--You will determine the names of the five countries and their respective regions with the lowest life expectancy for the year 2010. Use your knowledge about joins, filtering, sorting and limiting to create this list!
+
+SELECT c.name AS country,region,life_expectancy AS life_exp
+FROM countries AS c
+-- Join to populations (alias as p) using an appropriate join
+inner join populations as p 
+ON c.code = p.country_code
+-- Filter for only results in the year 2010
+where year ='2010'
+-- Sort by life_exp
+order by life_exp
+-- Limit to five records
+limit  5;
+
+
+-- Select aliased fields from populations as p1
+select p1.country_code,p1.size as size2010,p2.size as size2015
+-- Join populations as p1 to itself, alias as p2, on country code
+from populations as p1 
+inner join populations as p2
+on p1.country_code=p2.country_code
+-- Filter such that p1.year is always five years before p2.year
+and p1.year=p2.year-5;
+
